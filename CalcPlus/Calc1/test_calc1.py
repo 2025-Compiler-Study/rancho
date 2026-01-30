6a43a52f33c5b237096736b2085db5d3998dc298
jake <rancho974@gmail.com>
2026-01-30 18:55:34 +0900

    comment: calc#1 listener

--- CalcPlus/Calc1/test_calc1.py
+++ CalcPlus/Calc1/test_calc1.py
@@ -34,6 +34,17 @@ class CalcVisitorTest(unittest.TestCase):
             },
         )
 
+'''
+가능한 이유는 간단해. listener.warnings가 dict 리스트고, 각 dict가 {"line": ..., "column": ..., "name": ...} 구조를 보장하기 때문이야. 그래서 리스트 내포로 필요한 값만 추려서 튜플 리스트를 만들 수 있어.
+
+구체적으로:
+
+listener.warnings는 enterVar에서 warning = {"line": token.line, "column": token.column, "name": name} 형태로 추가됨
+그러니 warn["line"], warn["column"], warn["name"]는 항상 존재
+리스트 내포는 그 키들을 순서대로 튜플로 뽑아 [(line, column, name), ...]를 만든다
+이 결과를 self.assertEqual로 기대값과 비교하는 게 자연스러움
+즉, warnings의 자료구조가 정해져 있기 때문에 저렇게 꺼내도 안전하고, 테스트 의도(순서와 내용 비교)에도 맞아.
+'''
 class Calc1WarningListenerTest(unittest.TestCase):
     def test_use_before_definition(self):
         program = "\n".join(
