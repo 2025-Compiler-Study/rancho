72bf2542796ed03fbbc2506f7f280d56b507548d
jake <rancho974@gmail.com>
2026-02-09 21:10:51 +0900

    wip: Calc01 listener
    - listener DFS 순서 방문
    - 할당 연산 이전 exit() 메서드 처리 중
    - 할당 연산할 때 우변에 변수가 기존에 없으면 에러 처리 예정
    - 할당 연산 이후에 우변에 있는 값들이 계산된 다면 좌변에 있는 변수에 값을 할당함
      - 좌변에 있는 값이 변수가 아니라면 에러

--- CalcPlus/Calc1/test_calc1.py
+++ CalcPlus/Calc1/test_calc1.py
@@ -4,7 +4,8 @@ from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
 from CalcPlusLexer import CalcPlusLexer
 from CalcPlusParser import CalcPlusParser
 from CalcVisitor import CalcVisitor
-from calc1_warning_listener import Calc1WarningListener
+# from calc1_warning_listener import Calc1WarningListener
+from CalcListener import CalcListener
 
 def parse_program(program: str):
     input_stream = InputStream(program)
@@ -13,7 +14,7 @@ def parse_program(program: str):
     parser = CalcPlusParser(stream)
     return parser.calc1()
 
-
+'''
 class CalcVisitorTest(unittest.TestCase):
     def test_variable_memory(self):
         program = "A=1;\
@@ -33,6 +34,7 @@ class CalcVisitorTest(unittest.TestCase):
                 "e": 0,
             },
         )
+'''
 
 '''
 가능한 이유는 간단해. listener.warnings가 dict 리스트고, 각 dict가 {"line": ..., "column": ..., "name": ...} 구조를 보장하기 때문이야. 그래서 리스트 내포로 필요한 값만 추려서 튜플 리스트를 만들 수 있어.
@@ -45,33 +47,34 @@ listener.warnings는 enterVar에서 warning = {"line": token.line, "column": tok
 이 결과를 self.assertEqual로 기대값과 비교하는 게 자연스러움
 즉, warnings의 자료구조가 정해져 있기 때문에 저렇게 꺼내도 안전하고, 테스트 의도(순서와 내용 비교)에도 맞아.
 '''
-class Calc1WarningListenerTest(unittest.TestCase):
+class CalcListenerTest(unittest.TestCase):
     def test_use_before_definition(self):
         program = "\n".join(
             [
-                "a = b + 3;",
-                "c = a + d;",
-                "b = b + 1;",
+                # "a = b + 3;",
+                # "c = a + d;",
+                "b = b * 1;",
             ]
         )
+
         tree = parse_program(program)
-        listener = Calc1WarningListener()
+        listener = CalcListener()
         walker = ParseTreeWalker()
         walker.walk(listener, tree)
 
-        warnings = [
-            (warn["line"], warn["column"], warn["name"])
-            for warn in listener.warnings
-        ]
+        # warnings = [
+        #     (warn["line"], warn["column"], warn["name"])
+        #     for warn in listener.warnings
+        # ]
 
-        self.assertEqual(
-            warnings,
-            [
-                (1, 4, "b"),
-                (2, 8, "d"),
-                (3, 4, "b"),
-            ],
-        )
+        # self.assertEqual(
+        #     warnings,
+        #     [
+        #         (1, 4, "b"),
+        #         (2, 8, "d"),
+        #         (3, 4, "b"),
+        #     ],
+        # )
 
 if __name__ == "__main__":
     unittest.main()
